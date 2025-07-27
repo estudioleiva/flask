2c4d9c14f2f7bb118f4c9c5e96caa6346301fa2c
estudioleiva <105404641+estudioleiva@users.noreply.github.com>
2025-07-27 11:46:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚂"})
 
+@app.route("/ping")
+def ping():
+    return jsonify({"message": "pong"})
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
